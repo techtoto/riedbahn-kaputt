39f8e80f339cd9fb7b43a56de20424d14b72f2e5
Tobias Reichenbach <toto.rei0210@gmail.com>
2024-05-16 21:24:08 +0200

    first rudimentary disruption details

--- static/index.js
+++ static/index.js
@@ -21,8 +21,37 @@ socket.addEventListener("message", async event => {
 })
 
 function reloadStatus() {
-    if (disruptions)
+    if (disruptions) {
         document.getElementById("status").textContent = disruptions.length === 0 ? "Nein." : "Ja."
+
+        disruptions.forEach(element => {
+            const entry = document.createElement("div")
+            entry.classList.add("disruptionText")
+
+            const title = document.createElement("p")
+            title.classList.add("bold")
+            title.textContent = element.head
+
+            const date = document.createElement("p")
+            date.classList.add("italic")
+            date.textContent = `${element.zeitraum.beginn} - ${element.zeitraum.ende}`
+
+            const locations = document.createElement("p")
+            locations.classList.add("italic")
+            let locationsText = ""
+            locations.textContent = element.betriebsstellen.map(a => `${a.langname} (${a.ril100})`).join(", ")
+
+            const content = document.createElement("p")
+            content.textContent = element.text
+
+            entry.appendChild(title)
+            entry.appendChild(date)
+            entry.appendChild(locations)
+            entry.appendChild(content)
+
+            document.getElementById("disruptions").appendChild(entry)
+        })
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
